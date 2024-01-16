a6c3a8a42ec449c8f90ffe1e5f6588458e78269e
sunil barewar <sunilvbarewar@gmail.com>
2024-01-16 23:40:40 +0530

    redis-config

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const cron = require("node-cron");
+const { Redis } = require("ioredis");
+const { createAdapter } = require("@socket.io/redis-adapter");
 dotenv.config();
 
 const connectDB = require("./models");
@@ -11,7 +13,7 @@ const { errorHandler, notFound } = require("./middleware/error");
 const Routes = require("./routes");
 const sockets = require("./socket/sockets");
 
-const archieveOldMessages = require("./controllers/archievedMessages.controller");
+// const archieveOldMessages = require("./controllers/archievedMessages.controller");
 
 const app = express();
 
@@ -37,16 +39,50 @@ const server = app.listen(PORT, () =>
   console.log(`server running at PORT:${PORT} successfully`)
 );
 
+const redis = require("redis");
+const redisUri = process.env.REDIS_URI;
+const publisher = redis.createClient({
+  url: redisUri,
+});
+const subscriber = publisher.duplicate();
+
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     // credentials: true,
   },
 });
 
-io.on("connection", sockets);
+// io.adapter(createAdapter(pubClient, subClient));
+const constants = {
+  CHANNAL_NAME: "messages",
+};
 
-cron.schedule("0 0 * * *", async () => {
-  await archieveOldMessages();
+io.on("connection", async (socket) => {
+  // console.log("connected");
+  socket.on("join chat", (room) => {
+    socket.leaveAll();
+    // console.log("joined room: ", room);
+    socket.join(room);
+  });
+
+  socket.on("new.message", async (message) => {
+    // console.log("new message : ", message);
+    await publisher.publish(constants.CHANNAL_NAME, JSON.stringify(message));
+  });
 });
+
+(async () => {
+  await publisher.connect();
+  await subscriber.connect();
+
+  await subscriber.subscribe(constants.CHANNAL_NAME, (message) => {
+    const parsedMessage = JSON.parse(message);
+    io.in(parsedMessage.chatID).emit("new.message", parsedMessage);
+  });
+})();
+
+// cron.schedule("0 0 * * *", async () => {
+//   await archieveOldMessages();
+// });
--- utils/db-config.js
+++ utils/db-config.js
@@ -1,12 +1,19 @@
 const { Sequelize } = require("sequelize");
-
+const fs = require("fs");
+const path = require("path");
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
+    port: process.env.DB_PORT,
     dialect: "mysql",
     host: process.env.DB_HOST,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.join(__dirname, "../ca.pem")).toString(),
+      },
+    },
   }
 );
 
